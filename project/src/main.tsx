db6d3de381f0afcd5e8e8de951a4cd2cfbff5af9
hritick3138 <hritickkumar3138@gmail.com>
2026-09-11 11:12:08 +0530

    update index ,main

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -4,12 +4,16 @@ import App from './App.tsx';
 import './styles/index.css';
 import { BrowserRouter } from "react-router-dom";
 import { DeviceProvider } from './components/context/DeviceContext.tsx';
+import { HelmetProvider } from "react-helmet-async";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <DeviceProvider>
+      <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </HelmetProvider>
     </DeviceProvider>
   </StrictMode>
  
