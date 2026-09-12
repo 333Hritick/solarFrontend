a73197427a5d2f7ddf1cab5383935e7db86ea08e
hritick3138 <hritickkumar3138@gmail.com>
2026-09-12 11:25:03 +0530

    api.ts

--- project/src/api.ts
+++ project/src/api.ts
@@ -8,7 +8,7 @@ export const api = axios.create({
 
 // Auth
 export const registerUser = (data: any) => api.post("/register/", data);
-export const loginUser = (data: any) => api.post("/login/", data);
+export const loginUser = (data: any) => api.post("/token/", data);
 export const getProfile = (token: string) =>
   api.get("/profile/", { headers: { Authorization: `Bearer ${token}` } });
 
